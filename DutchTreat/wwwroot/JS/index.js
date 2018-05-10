848a45e243436b4ceab7161e1ef6151731e195c8
unknown <jluhbarnes86@gmail.com>
2018-05-10 14:27:12 -0400

    first commit

---
+++ DutchTreat/wwwroot/JS/index.js
@@ -0,0 +1,9 @@
+﻿var form = document.getElementById("form");
+var button = document.getElementById("buyButton");
+button.addEventListener("click", function () {
+
+    console.log("buying item");
+
+});
+
+var product = $("#product-props");
